1200d40241fe8b82b28bcd01efd76bdbc7e0a694
IN-BR-007 <IN-BR-007@IN-BR-007>
2019-03-12 16:20:04 +0100

    Signed-off-by: IN-BR-007 <IN-BR-007@IN-BR-007>

--- XX_Project_ECommerce/src/fr/admaing/dao/AdminDaoImpl.java
+++ XX_Project_ECommerce/src/fr/admaing/dao/AdminDaoImpl.java
@@ -1,5 +1,11 @@
 package fr.admaing.dao;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+@Stateless
 public class AdminDaoImpl implements IAdminDao{
 
+	@PersistenceContext(unitName="PU_ECommerce")
+	private EntityManager em;
 }
--- XX_Project_ECommerce/src/fr/admaing/dao/IAdminDao.java
+++ XX_Project_ECommerce/src/fr/admaing/dao/IAdminDao.java
@@ -1,5 +1,8 @@
 package fr.admaing.dao;
 
+import javax.ejb.Local;
+
+@Local
 public interface IAdminDao {
 
 }
