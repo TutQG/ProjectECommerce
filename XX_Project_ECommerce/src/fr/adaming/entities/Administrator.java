20ad5341dfa21cd4d69558267719e7082484aa77
IN-BR-007 <IN-BR-007@IN-BR-007>
2019-03-12 14:26:55 +0100

    ff

--- XX_Project_ECommerce/src/fr/adaming/entities/Administrator.java
+++ XX_Project_ECommerce/src/fr/adaming/entities/Administrator.java
@@ -2,4 +2,47 @@ package fr.adaming.entities;
 
 public class Administrator {
 
+	//déclaration des attributs
+	private int id;
+	private String mail;
+	private String pwd;
+	
+	//constructeurs
+	public Administrator() {
+		super();
+	}
+	public Administrator(String mail, String pwd) {
+		super();
+		this.mail = mail;
+		this.pwd = pwd;
+	}
+	public Administrator(int id, String mail, String pwd) {
+		super();
+		this.id = id;
+		this.mail = mail;
+		this.pwd = pwd;
+	}
+	
+	//getter et setter
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
 }
--- XX_Project_ECommerce/src/fr/adaming/entities/Product.java
+++ XX_Project_ECommerce/src/fr/adaming/entities/Product.java
@@ -2,4 +2,13 @@ package fr.adaming.entities;
 
 public class Product {
 
+	
+	//Déclaration des attributs
+	private int id;
+	private String designation;
+	private String description;
+	private double price;
+	private int quantity;
+	
+	
 }
