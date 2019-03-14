62c8d4d8ef3d3e3a139ef8f4ebe4c587aa17f79b
IN-BR-011 <IN-BR-011@IN-BR-011>
2019-03-14 14:03:41 +0100

    Signed-off-by: IN-BR-011 <IN-BR-011@IN-BR-011>

--- XX_Project_ECommerce/src/fr/adaming/dao/IOLDao.java
+++ XX_Project_ECommerce/src/fr/adaming/dao/IOLDao.java
@@ -1,5 +1,16 @@
 package fr.adaming.dao;
 
+import java.util.List;
+
+import fr.adaming.entities.OrderLine;
+
 public interface IOLDao {
 
+	public List<OrderLine> getAllOL(OrderLine ol);
+
+	public OrderLine addOL(OrderLine ol);
+
+	public int updateOL(OrderLine ol);
+
+	public int deleteOL(OrderLine ol);
 }
--- XX_Project_ECommerce/src/fr/adaming/dao/OLDaoImpl.java
+++ XX_Project_ECommerce/src/fr/adaming/dao/OLDaoImpl.java
@@ -1,5 +1,33 @@
 package fr.adaming.dao;
 
+import java.util.List;
+
+import fr.adaming.entities.OrderLine;
+
 public class OLDaoImpl implements IOLDao {
+
+	@Override
+	public List<OrderLine> getAllOL(OrderLine ol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderLine addOL(OrderLine ol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateOL(OrderLine ol) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteOL(OrderLine ol) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
